ebe3ec08ccadb2f04f5349a178e9b18a25b5ee61
criticalpixel <thomas@criticalpixel.com>
2017-02-07 00:05:22 +0000

    progress

---
+++ assets/code/angular.ts
@@ -0,0 +1,63 @@
+//Angular Bootstrap
+import { platformBrowserDynamic } from 
+'@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+
+
+//Angular Module
+import { NgModule } from '@angular/core';
+
+import { AppComponent }   from './app.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [AppComponent],
+  providers: [],
+})
+export class AppModule { }
+
+
+//Angular Component
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`
+})
+export class AppComponent { name = 'Angular'; }
+
+
+//Angular Route
+...imports...
+
+const routes: Routes = [
+  { path: 'path', component: AppComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild|Root(routes)],
+  exports: [RouterModule],
+})
+export class NameRoutingModule { }
+export const routedComponents = [AppComponent];
+
+
+
+
+
+
+
+
+
+
+
+
+
+ng serve
+ng build
+ng build --prod
+ng build --prod --aot
--- presentation/index.js
+++ presentation/index.js
@@ -3,15 +3,22 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  Spectacle,
+  Appear,
   BlockQuote,
   Cite,
+  CodePane,
   Deck,
+  Fill,
   Heading,
+  Image,
+  Layout,
+  Link,
   ListItem,
   List,
+  Markdown,
   Quote,
   Slide,
+  Spectacle,
   Text
 } from "spectacle";
 
@@ -26,23 +33,30 @@ import createTheme from "spectacle/lib/themes/default";
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
-
+// Fucken Typescriptnpm run
+require("prismjs");
+require("prismjs/components/prism-typescript");
 
 const images = {
   city: require("../assets/city.jpg"),
   kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  logo: require("../assets/angular_logo.svg"),
+  markdown: require("../assets/markdown.png"),
+  mainbg: require("../assets/main_bg.jpg"),
+  slide2: require("../assets/slide_2.jpg"),
+  then_vs_now : require("../assets/then_vs_now.jpg"),
+  slide3: require("../assets/slide_3.jpg")
 };
 
 preloader(images);
 
 const theme = createTheme({
   primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
+  secondary: "#42a5f7",
+  tertiary: "#134ca7",
   quartenary: "#CECECE",
-  codeBg: "#122b45",
+  dark: "#202020",
+  codeBg: "#122b45"
 }, {
   primary: "Montserrat",
   secondary: "Helvetica"
@@ -51,37 +65,57 @@ const theme = createTheme({
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck transitionDuration={500} theme={theme}>
+        <Slide transition={["zoom"]} bgImage={[images.mainbg]}>
+          <Image src={[images.logo]} width="30%" margin="-100px 0 0 0">
+          </Image>
+          <Heading size={1} fit caps lineHeight={1} textColor="primary">
+            Building Angular Apps
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="dark" size={3} bold>
+            Understanding the build process
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide transition={["f"]} bgImage={[images.slide2]}>
         </Slide>
+        <Slide transition={["fade"]} align="top" bgImage={[images.mainbg]}>
+          <Heading size={5} margin="60px 0 0 0" textAlign="left" textColor="primary">
+            Today I'm talking about...
+            </Heading>
+
+          <List textColor="dark" margin="60px 0 0 0">
+            <Appear><ListItem margin="20px 0 0 0">Angular workflow</ListItem></Appear>
+            <Appear><ListItem margin="20px 0 0 0">Breakdown of the build process</ListItem></Appear>
+            <Appear><ListItem margin="20px 0 0 0">Using Webpack save our lives</ListItem></Appear>
+            <Appear><ListItem margin="20px 0 0 0">Demo :)</ListItem></Appear>
+          </List>
+        </Slide>
+
+        <Slide transition={["fade"]} bgImage={[images.mainbg]}>
+          <Heading size={5} margin="60px 0 0 0" fit caps textColor="primary">
+            Our workflow has changed a bit.
+          </Heading>
+          <Text size={3}>
+            We can no longer just stick a .min.js file from a CDN and start developing.
+          </Text>
+           <Appear><Image src={[images.then_vs_now]} width="60%"></Image></Appear>
+        </Slide>
+
+        <Slide transition={["fade"]} bgImage={[images.slide3]}>
+        </Slide>
+
          <CodeSlide
             transition={["f"]}
-            lang="js"
+            lang="typescript"
             bgColor="codeBg"
-            code={require("raw-loader!../assets/code.example")}
+            code={require("raw-loader!../assets/code/angular.ts")}
             ranges={[
-              { loc: [0, 0], title: "Walking through some code" },
-              { loc: [0, 1], title: "The Beginning" },
-              { loc: [1, 2] },
-              { loc: [1, 2], note: "Heres a note!" },
-              { loc: [2, 3] },
-              { loc: [4, 7] },
-              { loc: [8, 10] }
+              { loc: [0, 0], title: "Step 1: Become a Hero" },
+              { loc: [0, 7], note: "We bootstrap the app" },
+              { loc: [10, 21], note: "We wrap our heads around modules" },
+              { loc: [24, 31], note: "We get busy with Components" },
+              { loc: [33, 48], note: "And we play with Routes" },
+              { loc: [59, 64], title: "Step 2: Angular CLI", note: "We use commands to get stuff done" }
               // ...
             ]}/>
 
@@ -96,8 +130,8 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>Thank You!</Quote>
+            <Cite>Thomas</Cite>
           </BlockQuote>
         </Slide>
       </Deck>
